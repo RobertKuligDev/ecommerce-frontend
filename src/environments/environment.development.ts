24d24770f29c599acf5a4357e27daaee0d1f2b48
RobertKuligDev <git@robertkulig-dev.eu>
2026-02-17 19:49:28 +0100

    feat(env): configure localhost for development environment
    - Set development API URL to http://localhost:5000/api/
    - Keep production URL pointing to api.robertkulig-dev.eu
    - Add stripe_publishable_key to production environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://api.robertkulig-dev.eu/api/',
-    stripe_publishable_key: 'pk_test_51QJvDoIzIiflPgpsRrYkeTVvxzOZLG3zY0KnDNE4pTuvK1WgUIoGIV5n9JlS7SXGbosS697mr6wojlL6Lp2eEDk400hAJQUAUg'
+  production: false,
+  apiUrl: 'http://localhost:5000/api/',
+  stripe_publishable_key: 'pk_test_51QJvDoIzIiflPgpsRrYkeTVvxzOZLG3zY0KnDNE4pTuvK1WgUIoGIV5n9JlS7SXGbosS697mr6wojlL6Lp2eEDk400hAJQUAUg'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.robertkulig-dev.eu/api/',
+  stripe_publishable_key: 'pk_test_51QJvDoIzIiflPgpsRrYkeTVvxzOZLG3zY0KnDNE4pTuvK1WgUIoGIV5n9JlS7SXGbosS697mr6wojlL6Lp2eEDk400hAJQUAUg'
 };
